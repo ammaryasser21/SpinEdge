32dd1aa42521204f62c064cfc1d55875d08f2dba
ammaryasser21 <ammaryasserqwertyu@gmail.com>
2024-07-23 15:23:07 +0300

    font solved

--- app/layout.js
+++ app/layout.js
@@ -3,17 +3,18 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import '../style/App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import './globals.css'
+import { Raleway } from 'next/font/google'
 import Head from 'next/head';
-
+const raleway = Raleway({ subsets: ['latin'] })
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
         <link rel="icon" href="/images/mainLogo.svg" />
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet" />
         <title>SpinEdge</title>
       </Head>
-      <body>
+      <body className={raleway.className}>
         <Navbar />
         <main className="container">{children}</main>
         <Footer />
